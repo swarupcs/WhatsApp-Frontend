0cd12e4ffb3197e45bd86bbf53ffadee8464929d
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-12-14 19:21:48 +0530

    setup tailwind classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
-
-
   return (
     <>
-    
+      <div class='bg-dark_bg_1 dark:bg-dark_bg_2'>Hello</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
