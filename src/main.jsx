af09aff98d1395b58dd4e69202fe77180a55082a
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-12-18 22:41:10 +0530

    creating chat slice

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { persistor, store } from './app/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
